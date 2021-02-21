618850de5a202ecf8c488f0facd21c7c6d807def
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2021-02-22 10:18:09 +1100

    Set up basic server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+const port = process.env.SERVER_PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Home page: "/"');
+});
+
+app.get('/help', (req, res) => {
+  res.send('Help page: "/help"');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact page: "/contact"');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`)
+});
